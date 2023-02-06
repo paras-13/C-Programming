78ca323429aad35829f856404c3f1139b40301a0
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-02-06 23:55:46 +0530

    C-programming day2

---
+++ prog2.c
@@ -0,0 +1,19 @@
+// Tokens in C
+/*
+1. --> Keywords:- Reserved by compiler. Their are 32 key-words
+2. --> Variables:- User defined,
+3. --> Constants:- Assigned to variables
+4. --> Operators:- Used in expressions
+5. --> Special Characters:- For different declarations in C
+6. --> Strings:- A sequence of characters
+*/
+// A single character is written under single quotes while a string is written in double quotes
+
+#include <stdio.h>
+int main(){
+    int b,c;
+    b=68;
+    c=72;
+    printf("%c %d",b,c);
+    return 0;
+}
