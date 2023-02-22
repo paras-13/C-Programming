d727fb01be10cb4863de662ff6d3061f03bfe0ff
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-02-23 01:26:02 +0530

    Equivalent form in octal and hexadecimal ( C practise)

---
+++ pract7.c
@@ -0,0 +1,12 @@
+// Write a program to enter an integer number and display its equivalent values in octal and hexadecimal.
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter an number =");
+    scanf("%d",&num);
+    printf("%d in octal is %o\n",num,num);          // %o for octal
+    printf("%d in hexadecimal is %X",num,num);      // %X for hexadecimal 
+    return 0;
+
+}
