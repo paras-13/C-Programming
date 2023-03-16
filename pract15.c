0f2ae92326c06f18b41130a44f197b50cab20084
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-03-16 19:07:21 +0530

    Check vowel, consonant or symbol using switch

---
+++ pract15.c
@@ -0,0 +1,71 @@
+// Write a program using switch case to recognize the entered character is vowel, consonants or symbol?
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Programming to check whether the entered character is vowel, conconant or symbol\n");
+    printf("Please enter a character\n");
+    scanf("%c",&c);
+    switch(c) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("%c is a vowel\n", c);
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+            printf("%c is a consonant\n", c);
+            break;
+        default:
+            printf("%c is a symbol\n", c);
+    }
+    return 0;
+}
